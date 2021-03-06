8d4bcbb2c55e3280bab87facc2ea1ba516f81d20
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-03-06 09:39:11 +0100

    views

--- routes/org.routes.js
+++ routes/org.routes.js
@@ -1,25 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const orgController = require('../controllers/org.controller/org.controller')
 const orgEditController = require('../controllers/org.controller/org.edit.controller')
 const orgSettingController = require('../controllers/org.controller/org.settings.controller')
 const secure = require('../middlewares/secure.middleware')
 const upload = require('../configs/storage.config')
 
 // Edit org ------------------------------------
-
-router.get ('/org/edit-profile',
+router.get(
+    '/org/edit-profile',
     secure.checkRoles('ORG'),
     secure.isAuthenticated,
     orgEditController.editProfile
 )
-router.post ('/edit-profile-org',
+router.post(
+    '/edit-profile-org',
     secure.checkRoles('ORG'),
     upload.single('profilePicture'),
     secure.isAuthenticated,
     orgEditController.doEditProfileOrg
 )
+
+// Public profile
+router.get('/org/:id', orgController.publicProfile)
+
 // Settings org ---------------------------------
-router.get ('/org/settings',
+router.get(
+    '/org/settings',
     secure.checkRoles('ORG'),
     secure.isAuthenticated,
     orgSettingController.settingsView
